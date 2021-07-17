8000d36122e4aa941f28b7cb25aa53ed817687dd
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-07-16 21:39:10 -0700

    Remove boost logging and gtest.

--- tfd_cpp/tfd.cpp
+++ tfd_cpp/tfd.cpp
@@ -1,27 +1,11 @@
 #include "tfd.h"
 #include <iostream>
-#include <boost/log/core.hpp>
-#include <boost/log/trivial.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/log/sinks/text_file_backend.hpp>
-#include <boost/log/utility/setup/file.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
-#include <boost/log/sources/severity_logger.hpp>
-#include <boost/log/sources/record_ostream.hpp>
 
 namespace tfd_cpp
 {
-    namespace logging = boost::log;
-
     TFD::TFD(const PlanningProblem& planningProblem) : 
         m_planningProblem(planningProblem)
     {
-        logging::add_file_log("tfd_cpp.log");
-
-        logging::core::get()->set_filter
-        (
-            logging::trivial::severity >= logging::trivial::trace
-        );
     }
 
     TFD::~TFD() {}
@@ -32,7 +16,7 @@ namespace tfd_cpp
         Plan solutionPlan;
         tasks.push_back(m_planningProblem.GetTopLevelTask());
 
-        BOOST_LOG_TRIVIAL(info) << "TryToPlan for: " << tasks.back().taskName;
+        std::cout << "TryToPlan for: " << tasks.back().taskName;
 
         return SeekPlan(tasks, m_planningProblem.GetInitialState(), solutionPlan);
     }
@@ -41,13 +25,13 @@ namespace tfd_cpp
     {
         if (tasks.empty())
         {
-            BOOST_LOG_TRIVIAL(info) << "SeekPlan: No more tasks, returning current plan.";
+            std::cout << "SeekPlan: No more tasks, returning current plan.\n";
             if (not currentPlan.empty())
             {
-                BOOST_LOG_TRIVIAL(info) << "TFD found solution plan." << std::endl;
+                std::cout << "TFD found solution plan." << std::endl;
                 for (const auto& operatorWithParams : currentPlan)
                 {
-                    BOOST_LOG_TRIVIAL(info) << operatorWithParams.task.taskName;
+                    std::cout << operatorWithParams.task.taskName;
                 }
             }
             return currentPlan;
@@ -55,13 +39,13 @@ namespace tfd_cpp
 
         if (m_planningProblem.TaskIsOperator(tasks.back().taskName))
         {
-            BOOST_LOG_TRIVIAL(trace) << "SeekPlan: Task is operator type.";
+            std::cout << "SeekPlan: Task is operator type.\n";
             return SearchOperators(tasks, currentState, currentPlan);
         }
 
         if (m_planningProblem.TaskIsMethod(tasks.back().taskName))
         {
-            BOOST_LOG_TRIVIAL(trace) << "SeekPlan: Task is method type.";
+            std::cout << "SeekPlan: Task is method type.\n";
             return SearchMethods(tasks, currentState, currentPlan);
         }
 
@@ -70,12 +54,12 @@ namespace tfd_cpp
 
     TFD::Plan TFD::SearchMethods(const std::vector<Task>& tasks, const State& currentState, Plan& currentPlan)
     {
-        BOOST_LOG_TRIVIAL(trace) << "SearchMethods for " << tasks.back().taskName;
+        std::cout << "SearchMethods for " << tasks.back().taskName;
         RelevantMethods relevantMethods = m_planningProblem.GetMethodsForTask(tasks.back(), currentState);
         
         if (not relevantMethods.empty())
         {
-            BOOST_LOG_TRIVIAL(trace) << "SearchMethods: " << relevantMethods.size() <<  " relevant methods found.";
+            std::cout << "SearchMethods: " << relevantMethods.size() <<  " relevant methods found.\n";
 
             for (const auto& relevantMethod : relevantMethods)
             {
@@ -96,16 +80,16 @@ namespace tfd_cpp
         }
         else
         {
-            BOOST_LOG_TRIVIAL(info) << "SearchMethods: No relevant methods found.";
+            std::cout << "SearchMethods: No relevant methods found.\n";
         }
 
-        BOOST_LOG_TRIVIAL(warning) << "SearchMethods: Failed to plan";
+        std::cout << "SearchMethods: Failed to plan";
         return {};
     }
 
     TFD::Plan TFD::SearchOperators(const std::vector<Task>& tasks, const State& currentState, Plan& currentPlan)
     {
-        BOOST_LOG_TRIVIAL(trace) << "SearchOperators for " << tasks.back().taskName;
+        std::cout << "SearchOperators for " << tasks.back().taskName;
         ApplicableOperators applicableOperators = m_planningProblem.GetOperatorsForTask(tasks.back(), currentState);
         
         if (not applicableOperators.empty())
@@ -130,7 +114,7 @@ namespace tfd_cpp
         }
         else
         {
-            BOOST_LOG_TRIVIAL(warning) << "SearchOperators: No applicable operator found.";
+            std::cout << "SearchOperators: No applicable operator found.\n";
         }
 
         return {};
