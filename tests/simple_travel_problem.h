b83680dfe24c4702d6b4eef96306459e7c3502e9
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-07-16 22:47:08 -0700

    Collapse tests.

--- tests/simple_travel_problem.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <memory>
-#include <vector>
-
-#include <modules/tfd/planning_domain.h>
-#include <modules/tfd/planning_problem.h>
-
-PlanningProblem CreatePlanningProblem(const Task& topLevelTask);
--- tests/test_tfd.h
+++ tests/test_tfd.h
@@ -36,12 +36,383 @@
 #include <functional>
 #include <optional>
 
-#include <modules/tfd/tests/simple_travel_domain.h>
-#include <modules/tfd/tests/simple_travel_problem.h>
+#include <modules/tfd/planning_problem.h>
 #include <modules/tfd/tfd.h>
-
+#include <cassert>
+#include <sstream>
 
 namespace TestTFD {
+
+// Simple Travel Planning Domain
+#pragma once
+
+#include <functional>
+
+#include <modules/tfd/planning_domain.h>
+
+#define DOMAIN_NAME "simple_travel"
+#define WALKING_DISTANCE 2
+
+// Tasks
+#define TRAVEL "Travel"
+#define WALK "Walk"
+#define CALL_TAXI "CallTaxi"
+#define RIDE_TAXI "RideTaxi"
+#define PAY_DRIVER "PayDriver"
+
+class SimpleTravelState {
+public:
+	using Object = std::string;
+	using Location = std::string;
+	using Cash = std::size_t;
+	using Distance = std::size_t;
+
+	using PersonLocationTable = std::map<Object, Location>;
+	using PersonCashTable = std::map<Object, Cash>;
+	using PersonOweTable = std::map<Object, Cash>;
+	using DistanceTable = std::map<Location, std::map<Location, Distance>>;
+
+	SimpleTravelState(const PersonLocationTable &personLocationTable,
+			const PersonCashTable &personCashTable,
+			const PersonOweTable &personOweTable,
+			const DistanceTable &distanceTable);
+	virtual ~SimpleTravelState();
+
+	std::optional<Location> LocationOf(const Object &person) const;
+	std::optional<Cash> CashOwnedBy(const Object &person) const;
+	std::optional<Cash> Owe(const Object &person) const;
+	std::optional<Distance> DistanceBetween(const Location &location1, const Location &location2) const;
+
+	void SetLocationOf(const Object &person, const Location &location);
+	void SetCashOwnedBy(const Object &person, const Cash &cash);
+	void SetOwe(const Object &person, const Cash &cash);
+
+	Cash TaxiRate(const Distance &distance) const;
+
+private:
+	friend std::ostream &operator<<(std::ostream &os, const SimpleTravelState &state);
+
+	PersonLocationTable m_personLocationTable;
+	PersonCashTable m_personCashTable;
+	PersonOweTable m_personOweTable;
+	DistanceTable m_distanceTable;
+};
+
+// Operators
+std::optional<State> Walk(const State &state, const Parameters &parameters);
+std::optional<State> CallTaxi(const State &state, const Parameters &parameters);
+std::optional<State> RideTaxi(const State &state, const Parameters &parameters);
+std::optional<State> PayDriver(const State &state, const Parameters &parameters);
+
+// Methods
+std::optional<std::vector<Task>> TravelByFoot(const State &state, const Parameters &parameters);
+std::optional<std::vector<Task>> TravelByTaxi(const State &state, const Parameters &parameters);
+
+PlanningDomain CreatePlanningDomain();
+
+std::ostream &operator<<(std::ostream &os, const SimpleTravelState &state);
+
+// initial state
+static const SimpleTravelState::PersonLocationTable s_initPersonLocationTable = { { "me", "home" }, { "taxi", "park" } };
+static const SimpleTravelState::PersonCashTable s_initPersonCashTable = { { "me", 20 } };
+static const SimpleTravelState::PersonOweTable s_initPersonOweTable = { { "me", 0 } };
+static const SimpleTravelState::DistanceTable s_initDistanceTable = { { "home", { { "park", 8 } } }, { "park", { { "home", 8 } } } };
+
+static const SimpleTravelState s_initialState(s_initPersonLocationTable,
+		s_initPersonCashTable,
+		s_initPersonOweTable,
+		s_initDistanceTable);
+
+PlanningProblem CreatePlanningProblem(const Task &topLevelTask) {
+	auto planningDomain = CreatePlanningDomain();
+	State state = { DOMAIN_NAME, s_initialState };
+
+	return PlanningProblem(planningDomain, state, topLevelTask);
+}
+PlanningDomain CreatePlanningDomain() {
+	PlanningDomain planningDomain(DOMAIN_NAME);
+
+	// Add operators
+	planningDomain.AddOperator(WALK, Walk);
+	planningDomain.AddOperator(CALL_TAXI, CallTaxi);
+	planningDomain.AddOperator(RIDE_TAXI, RideTaxi);
+	planningDomain.AddOperator(PAY_DRIVER, PayDriver);
+
+	// Add methods
+	planningDomain.AddMethod(TRAVEL, TravelByFoot);
+	planningDomain.AddMethod(TRAVEL, TravelByTaxi);
+
+	return planningDomain;
+}
+
+SimpleTravelState::SimpleTravelState(const PersonLocationTable &personLocationTable,
+		const PersonCashTable &personCashTable,
+		const PersonOweTable &personOweTable,
+		const DistanceTable &distanceTable) :
+		m_personLocationTable(personLocationTable),
+		m_personCashTable(personCashTable),
+		m_personOweTable(personOweTable),
+		m_distanceTable(distanceTable) {
+}
+
+SimpleTravelState::~SimpleTravelState() {
+}
+
+std::optional<SimpleTravelState::Location> SimpleTravelState::LocationOf(const Object &person) const {
+	if (m_personLocationTable.find(person) == m_personLocationTable.end()) {
+		return std::nullopt;
+	} else {
+		return m_personLocationTable.at(person);
+	}
+}
+
+void SimpleTravelState::SetLocationOf(const Object &person, const Location &location) {
+	if (m_personLocationTable.find(person) != m_personLocationTable.end()) {
+		m_personLocationTable[person] = location;
+	}
+}
+
+std::optional<SimpleTravelState::Cash> SimpleTravelState::CashOwnedBy(const Object &person) const {
+	if (m_personCashTable.find(person) == m_personCashTable.end()) {
+		return std::nullopt;
+	} else {
+		return m_personCashTable.at(person);
+	}
+}
+
+void SimpleTravelState::SetCashOwnedBy(const Object &person, const Cash &cash) {
+	if (m_personCashTable.find(person) != m_personCashTable.end()) {
+		m_personCashTable[person] = cash;
+	}
+}
+
+std::optional<SimpleTravelState::Cash> SimpleTravelState::Owe(const Object &person) const {
+	if (m_personOweTable.find(person) == m_personOweTable.end()) {
+		return std::nullopt;
+	} else {
+		return m_personOweTable.at(person);
+	}
+}
+
+void SimpleTravelState::SetOwe(const Object &person, const Cash &cash) {
+	if (m_personOweTable.find(person) != m_personOweTable.end()) {
+		m_personOweTable[person] = cash;
+	}
+}
+
+std::optional<SimpleTravelState::Distance> SimpleTravelState::DistanceBetween(const Location &location1, const Location &location2) const {
+	if (m_distanceTable.find(location1) == m_distanceTable.end()) {
+		return std::nullopt;
+	} else {
+		const auto &destination_table = m_distanceTable.at(location1);
+		if (destination_table.find(location2) == destination_table.end()) {
+			return std::nullopt;
+		} else {
+			return destination_table.at(location2);
+		}
+	}
+}
+
+SimpleTravelState::Cash SimpleTravelState::TaxiRate(const Distance &distance) const {
+	return (1.5 + 0.5 * distance);
+}
+
+// Operators
+std::optional<State> Walk(const State &state, const Parameters &parameters) {
+	assert(parameters.size() == 3);
+	assert(state.domainName == DOMAIN_NAME);
+
+	try {
+		const auto &person = std::any_cast<SimpleTravelState::Object>(parameters[0]);
+		const auto &src = std::any_cast<SimpleTravelState::Location>(parameters[1]);
+		const auto &dst = std::any_cast<SimpleTravelState::Location>(parameters[2]);
+		auto simpleTravelState = std::any_cast<SimpleTravelState>(state.data);
+
+		auto currentLocation = simpleTravelState.LocationOf(person);
+		if (currentLocation && currentLocation.value() == src) {
+			State newState(state);
+			simpleTravelState.SetLocationOf(person, dst);
+			newState.data = simpleTravelState;
+
+			return newState;
+		}
+	} catch (const std::bad_any_cast &e) {
+		std::cerr << e.what() << " in " << __PRETTY_FUNCTION__ << '\n';
+	}
+
+	return std::nullopt;
+}
+
+std::optional<State> CallTaxi(const State &state, const Parameters &parameters) {
+	assert(parameters.size() == 2);
+	assert(state.domainName == DOMAIN_NAME);
+
+	try {
+		State newState(state);
+		auto simpleTravelState = std::any_cast<SimpleTravelState>(newState.data);
+		const auto &person = std::any_cast<SimpleTravelState::Object>(parameters[0]);
+		const auto &taxi = std::any_cast<SimpleTravelState::Object>(parameters[1]);
+
+		auto personLocation = simpleTravelState.LocationOf(person);
+		if (personLocation) {
+			simpleTravelState.SetLocationOf(taxi, personLocation.value());
+		}
+		newState.data = simpleTravelState;
+
+		return newState;
+	} catch (const std::exception &e) {
+		std::cerr << e.what() << " in " << __PRETTY_FUNCTION__ << '\n';
+	}
+
+	return std::nullopt;
+}
+
+std::optional<State> RideTaxi(const State &state, const Parameters &parameters) {
+	assert(parameters.size() == 4);
+	assert(state.domainName == DOMAIN_NAME);
+
+	try {
+		const auto &person = std::any_cast<SimpleTravelState::Object>(parameters[0]);
+		const auto &taxi = std::any_cast<SimpleTravelState::Object>(parameters[1]);
+		const auto &src = std::any_cast<SimpleTravelState::Location>(parameters[2]);
+		const auto &dst = std::any_cast<SimpleTravelState::Location>(parameters[3]);
+		auto simpleTravelState = std::any_cast<SimpleTravelState>(state.data);
+
+		auto currentPersonLocation = simpleTravelState.LocationOf(person);
+		auto currentTaxiLocation = simpleTravelState.LocationOf(taxi);
+
+		if (currentPersonLocation and currentTaxiLocation) {
+			if ((currentPersonLocation.value() == src) and (currentTaxiLocation.value() == src)) {
+				State newState(state);
+				auto distance = simpleTravelState.DistanceBetween(src, dst);
+
+				if (distance) {
+					simpleTravelState.SetLocationOf(person, dst);
+					simpleTravelState.SetLocationOf(taxi, dst);
+					simpleTravelState.SetOwe(person, simpleTravelState.TaxiRate(distance.value()));
+					newState.data = simpleTravelState;
+				}
+
+				return newState;
+			}
+		}
+	} catch (const std::exception &e) {
+		std::cerr << e.what() << " in " << __PRETTY_FUNCTION__ << '\n';
+	}
+
+	return std::nullopt;
+}
+
+std::optional<State> PayDriver(const State &state, const Parameters &parameters) {
+	assert(parameters.size() == 1);
+	assert(state.domainName == DOMAIN_NAME);
+
+	try {
+		const auto &person = std::any_cast<SimpleTravelState::Object>(parameters[0]);
+		auto simpleTravelState = std::any_cast<SimpleTravelState>(state.data);
+
+		auto cashOwned = simpleTravelState.CashOwnedBy(person);
+		auto owe = simpleTravelState.Owe(person);
+
+		if (cashOwned and owe) {
+			if (cashOwned.value() >= owe.value()) {
+				State newState(state);
+
+				simpleTravelState.SetCashOwnedBy(person, cashOwned.value() - owe.value());
+				simpleTravelState.SetOwe(person, 0);
+				newState.data = state;
+
+				return newState;
+			}
+		}
+	} catch (const std::exception &e) {
+		std::cerr << e.what() << " in " << __PRETTY_FUNCTION__ << '\n';
+	}
+
+	return std::nullopt;
+}
+
+// Methods
+std::optional<std::vector<Task>> TravelByFoot(const State &state, const Parameters &parameters) {
+	assert(parameters.size() == 4);
+	assert(state.domainName == DOMAIN_NAME);
+
+	try {
+		const auto &person = std::any_cast<SimpleTravelState::Object>(parameters[0]);
+		const auto &taxi = std::any_cast<SimpleTravelState::Object>(parameters[1]);
+		const auto &src = std::any_cast<SimpleTravelState::Location>(parameters[2]);
+		const auto &dst = std::any_cast<SimpleTravelState::Location>(parameters[3]);
+		auto simpleTravelState = std::any_cast<SimpleTravelState>(state.data);
+
+		auto distance = simpleTravelState.DistanceBetween(src, dst);
+		if (distance && distance.value() <= WALKING_DISTANCE) {
+			Task task;
+			task.taskName = WALK;
+			task.parameters.push_back(person);
+			task.parameters.push_back(src);
+			task.parameters.push_back(dst);
+			std::vector<Task> subtasks{ task };
+
+			return subtasks;
+		}
+	} catch (const std::exception &e) {
+		std::cerr << e.what() << " in " << __PRETTY_FUNCTION__ << '\n';
+	}
+
+	return std::nullopt;
+}
+
+std::optional<std::vector<Task>> TravelByTaxi(const State &state, const Parameters &parameters) {
+	assert(parameters.size() == 4);
+	assert(state.domainName == DOMAIN_NAME);
+
+	try {
+		const auto &person = std::any_cast<SimpleTravelState::Object>(parameters[0]);
+		const auto &taxi = std::any_cast<SimpleTravelState::Object>(parameters[1]);
+		const auto &src = std::any_cast<SimpleTravelState::Location>(parameters[2]);
+		const auto &dst = std::any_cast<SimpleTravelState::Location>(parameters[3]);
+		auto simpleTravelState = std::any_cast<SimpleTravelState>(state.data);
+
+		auto cash = simpleTravelState.CashOwnedBy(person);
+		auto distance = simpleTravelState.DistanceBetween(src, dst);
+		auto currentPersonLocation = simpleTravelState.LocationOf(person);
+		if (cash and distance and currentPersonLocation) {
+			if (currentPersonLocation.value() == src) {
+				if (cash.value() >= simpleTravelState.TaxiRate(distance.value())) {
+					Task callTaxi;
+					callTaxi.taskName = CALL_TAXI;
+					callTaxi.parameters.push_back(person);
+					callTaxi.parameters.push_back(taxi);
+
+					Task rideTaxi;
+					rideTaxi.taskName = RIDE_TAXI;
+					rideTaxi.parameters.push_back(person);
+					rideTaxi.parameters.push_back(taxi);
+					rideTaxi.parameters.push_back(src);
+					rideTaxi.parameters.push_back(dst);
+
+					Task payDriver;
+					payDriver.taskName = PAY_DRIVER;
+					payDriver.parameters.push_back(person);
+
+					std::vector<Task> subtasks{ payDriver, rideTaxi, callTaxi };
+
+					return subtasks;
+				}
+			}
+		}
+	} catch (const std::exception &e) {
+		std::cerr << e.what() << " in " << __PRETTY_FUNCTION__ << '\n';
+	}
+
+	return std::nullopt;
+}
+
+std::ostream &operator<<(std::ostream &os, const SimpleTravelState &state) {
+	os << "SimpleTravelState";
+	return os;
+}
+
 TEST_CASE("[Modules][TotalOrderForwardDecomposition] Simple Travel Problem") {
 	Task task;
 	task.taskName = TRAVEL;
@@ -49,7 +420,6 @@ TEST_CASE("[Modules][TotalOrderForwardDecomposition] Simple Travel Problem") {
 	task.parameters.push_back(SimpleTravelState::Object("taxi"));
 	task.parameters.push_back(SimpleTravelState::Location("home"));
 	task.parameters.push_back(SimpleTravelState::Location("park"));
-
 	TotalOrderForwardDecomposition tfd(CreatePlanningProblem(task));
 	TotalOrderForwardDecomposition::Plan solutionPlan = tfd.TryToPlan();
 
