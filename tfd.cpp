31da7b5dba11e57c57d407283639585088255395
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-07-23 01:02:15 -0700

    Move prints to verbose.

--- tfd.cpp
+++ tfd.cpp
@@ -1,6 +1,6 @@
 #include "tfd.h"
-#include <iostream>
-#include <ostream>
+#include "core/string/print_string.h"
+#include "core/variant/variant.h"
 
 TotalOrderForwardDecomposition::TotalOrderForwardDecomposition(const PlanningProblem &planningProblem) :
 		m_planningProblem(planningProblem) {
@@ -13,30 +13,30 @@ TotalOrderForwardDecomposition::Plan TotalOrderForwardDecomposition::try_to_plan
 	Plan solutionPlan;
 	tasks.push_back(m_planningProblem.get_top_level_task());
 
-	std::cout << "Try To Plan for: " << tasks.back().task_name << "\n";
+	print_verbose(vformat("Try To Plan for: %s\n", tasks.back().task_name.c_str()));
 
 	return seek_plan(tasks, m_planningProblem.get_initial_state(), solutionPlan);
 }
 
 TotalOrderForwardDecomposition::Plan TotalOrderForwardDecomposition::seek_plan(const std::vector<Task> &tasks, const State &p_current_state, Plan &currentPlan) {
 	if (tasks.empty()) {
-		std::cout << "SeekPlan: No more tasks, returning current plan.\n";
+		print_verbose("SeekPlan: No more tasks, returning current plan.\n");
 		if (!currentPlan.empty()) {
-			std::cout << "TFD found solution plan." << std::endl;
+			print_verbose("TFD found solution plan.");
 			for (const OperatorWithParams &operatorWithParams : currentPlan) {
-				std::cout << operatorWithParams.task.task_name;
+				print_verbose(operatorWithParams.task.task_name.c_str());
 			}
 		}
 		return currentPlan;
 	}
 
 	if (m_planningProblem.task_is_operator(tasks.back().task_name)) {
-		std::cout << "SeekPlan: Task is operator type.\n";
+		print_verbose("SeekPlan: Task is operator type.\n");
 		return search_operators(tasks, p_current_state, currentPlan);
 	}
 
 	if (m_planningProblem.task_is_method(tasks.back().task_name)) {
-		std::cout << "SeekPlan: Task is method type.\n";
+		print_verbose("SeekPlan: Task is method type.\n");
 		return search_methods(tasks, p_current_state, currentPlan);
 	}
 
@@ -44,11 +44,11 @@ TotalOrderForwardDecomposition::Plan TotalOrderForwardDecomposition::seek_plan(c
 }
 
 TotalOrderForwardDecomposition::Plan TotalOrderForwardDecomposition::search_methods(const std::vector<Task> &tasks, const State &p_current_state, Plan &currentPlan) {
-	std::cout << "SearchMethods for " << tasks.back().task_name;
+	print_verbose(vformat("SearchMethods for %s", tasks.back().task_name.c_str()));
 	RelevantMethods relevantMethods = m_planningProblem.get_methods_for_task(tasks.back(), p_current_state);
 
 	if (!relevantMethods.empty()) {
-		std::cout << "SearchMethods: " << relevantMethods.size() << " relevant methods found.\n";
+		print_verbose(vformat("SearchMethods: %d relevant methods found.", relevantMethods.size()));
 
 		for (const MethodWithParams &relevantMethod : relevantMethods) {
 			std::optional<std::vector<Task>> subTasks = relevantMethod.func(p_current_state, relevantMethod.task.parameters);
@@ -64,15 +64,15 @@ TotalOrderForwardDecomposition::Plan TotalOrderForwardDecomposition::search_meth
 			}
 		}
 	} else {
-		std::cout << "SearchMethods: No relevant methods found.\n";
+		print_verbose("SearchMethods: No relevant methods found.");
 	}
 
-	std::cout << "SearchMethods: Failed to plan";
+	print_verbose("SearchMethods: Failed to plan");
 	return {};
 }
 
 TotalOrderForwardDecomposition::Plan TotalOrderForwardDecomposition::search_operators(const std::vector<Task> &tasks, const State &p_current_state, Plan &currentPlan) {
-	std::cout << "SearchOperators for " << tasks.back().task_name;
+	print_verbose(vformat("SearchOperators for %s", tasks.back().task_name.c_str()));
 	ApplicableOperators applicableOperators = m_planningProblem.get_operators_for_task(tasks.back(), p_current_state);
 
 	if (!applicableOperators.empty()) {
@@ -91,7 +91,7 @@ TotalOrderForwardDecomposition::Plan TotalOrderForwardDecomposition::search_oper
 			}
 		}
 	} else {
-		std::cout << "SearchOperators: No applicable operator found.\n";
+		print_verbose("SearchOperators: No applicable operator found.");
 	}
 
 	return {};
