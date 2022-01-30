8bbe2784a42d190dd092df8ba7f9aa0eb336d13d
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-01-30 11:58:51 -0800

    Run code format.

--- register_types.cpp
+++ register_types.cpp
@@ -1,12 +1,11 @@
 #include "register_types.h"
-#include "planning.h"
 #include "core/object/class_db.h"
+#include "planning.h"
+
 
 void register_tfd_types() {
 	GDREGISTER_CLASS(TaskPlanner);
 }
 
 void unregister_tfd_types() {
-    
 }
-
